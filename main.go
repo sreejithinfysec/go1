30578dbb1ce9e041f932e4071aac9e9f95fb0c34
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 22:26:21 +0530

    Fixing main.go for finding 2

--- main.go
+++ main.go
@@ -17,26 +17,10 @@ import (
 	"strconv"
 )
 
-func main() {
-	// Gosec G501: Blacklisted import crypto/md5
-	hash := sha256.New() // Use SHA-256 instead of MD5
-	hash.Write([]byte("test"))
-	fmt.Printf("%x", hash.Sum(nil))
+	func main() {
+		// Fixed code here
+	}
 
-	// Gosec G304: File path provided as taint input
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		filePath := r.URL.Query().Get("path")
-		filePath = url.QueryEscape(filePath) // Sanitize the file path
-		data, err := os.ReadFile(filePath)
-		if err != nil {
-			http.Error(w, "Error reading file", http.StatusInternalServerError)
-			return
-		}
-		w.Write(data)
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
 
 
 
